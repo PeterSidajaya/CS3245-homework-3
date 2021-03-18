5830370a9f223b3afed4c858a6b2e0001c86551d
Peter <Peter@MacBook-Pro.local>
2021-03-18 17:48:55 +0800

    change posting list indexing to the new vector model

--- spimi.py
+++ spimi.py
@@ -1,97 +1,22 @@
 import pickle
 import os
 
-def invert(word_list, dictionary_file, posting_file):
-    """invert a word stream to dictionary and posting files
+def count_word(token_list):
+    """convert a stream of tokens into list of dictionary of term, frequency pairs
 
     Args:
-        word_list (list): word stream 
-        dictionary_file (str): address of the dictionary file
-        posting_file (str): address of the posting file
+        token_list (list): list of tokens
+
+    Returns:
+        dict: dictionary of term, frequency pairs
     """
-    dictionary = {}
-    for word, posting in word_list:
-        if word not in dictionary:
-            posting_list = [posting]
-            dictionary[word] = posting_list
+    count = {}
+    for term in token_list:
+        if term not in count:
+            count[term] = 1
         else:
-            posting_list = dictionary[word]
-        
-        if posting_list[-1] != posting:
-            posting_list.append(posting) # this will also modified the original dictionary
-    
-    dictionary_file = open(dictionary_file, 'wb')
-    posting_file = open(posting_file, 'wb')
-    # these two lines are for clarity purposes only
-    dictionary_file.truncate(0)
-    posting_file.truncate(0)
-
-    for word, posting_list in dictionary.items():
-        pointer = posting_file.tell()
-        pickle.dump(posting_list, posting_file)
-        dictionary[word] = (len(posting_list), pointer)
-
-    pickle.dump(dictionary, dictionary_file)
-
-
-def merge_files(dictionary_file_1_address, posting_file_1_address, dictionary_file_2_address, posting_file_2_address,
-    output_dictionary_address, output_posting_address):
-    """merge two pairs of dictionary and posting files
-
-    Args:
-        dictionary_file_1_address (str): address of the first dictionary file
-        posting_file_1_address (str): address of the first posting file
-        dictionary_file_2_address (str): address of the second dictionary file
-        posting_file_2_address (str): address of the second posting file
-        output_dictionary_address (str): address of the output dictionary file
-        output_posting_address (str): address of the output posting file
-    """
-    infile = open(dictionary_file_1_address, 'rb')
-    dictionary_file_1 = pickle.load(infile)
-    infile.close()
-    infile = open(dictionary_file_2_address, 'rb')
-    dictionary_file_2 = pickle.load(infile)
-    infile.close()
-
-    posting_file_1 = open(posting_file_1_address, 'rb')
-    posting_file_2 = open(posting_file_2_address, 'rb')
-
-    dictionary_output = open(output_dictionary_address, 'wb')
-    posting_output = open(output_posting_address, 'wb')
-    # these two lines are for clarity purposes only
-    dictionary_output.truncate(0)
-    posting_output.truncate(0)
-
-    dictionary = {}
-
-    for word in dictionary_file_1.keys():
-        posting_file_1.seek(dictionary_file_1[word][1])
-        posting_data_1 = pickle.load(posting_file_1)
-        if word in dictionary_file_2:
-            posting_file_2.seek(dictionary_file_2[word][1])
-            posting_data_2 = pickle.load(posting_file_2)
-            posting_list = merge_lists(posting_data_1, posting_data_2)
-        else:
-            posting_list = posting_data_1
-        pointer = posting_output.tell()
-        pickle.dump(posting_list, posting_output)
-        dictionary[word] = (len(posting_list), pointer)
-    
-    # for terms in dictionary_file_2 but not in dictionary_file_1
-    for word in dictionary_file_2.keys():
-        if word not in dictionary:
-            posting_file_2.seek(dictionary_file_2[word][1])
-            posting_list = pickle.load(posting_file_2)
-            pointer = posting_output.tell()
-            pickle.dump(posting_list, posting_output)
-            dictionary[word] = (len(posting_list), pointer)
-    
-    pickle.dump(dictionary, dictionary_output)
-    # remove the old files
-    os.remove(dictionary_file_1_address)
-    os.remove(dictionary_file_2_address)
-    os.remove(posting_file_1_address)
-    os.remove(posting_file_2_address)
+            count[term] += 1
+    return count
 
 
 def merge_lists(list_1, list_2):
